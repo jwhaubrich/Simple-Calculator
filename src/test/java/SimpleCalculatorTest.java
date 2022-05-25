d68a61346b5172c2bb2278a900a96b9f92e0d965
Joe Haubrich <jwhaubrich@gmail.com>
2022-05-25 07:52:35 -0500

    updated SimpleCalculator tests to assert True and print out True/False w/ sout

--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -12,41 +12,58 @@ class SimpleCalculatorTest {
     @Test
     void testAdditionShouldReturnFour() {
         int result;
+        int expectedResult = 4;
+        boolean resultEqualsExpected;
         Addition additionTest = new Addition();
-        result = additionTest.performOperation(2,2);
 
-        Assertions.assertEquals(4,result);
+        result = additionTest.performOperation(2,2);
+        resultEqualsExpected = (expectedResult == result);
+
+        Assertions.assertTrue(expectedResult == result);
+        System.out.println("Addition test 2 + 2 equals 4: "+resultEqualsExpected);
     }
 
 
     @Test
     void testSubtractionShouldReturnThree() {
         int result;
+        int expectedResult = 3;
+        boolean resultEqualsExpected;
 
         Subtraction subtractionTest = new Subtraction();
         result = subtractionTest.performOperation(6,3);
+        resultEqualsExpected = (expectedResult == result);
 
-        Assertions.assertEquals(3,result);
+        Assertions.assertTrue(expectedResult == result);
+        System.out.println("Subtraction test 6 - 3 equals 3: "+resultEqualsExpected);
     }
 
     @Test
     void testSubtractionShouldReturnNegativeFive() {
         int result;
+        int expectedResult = -5;
+        boolean resultEqualsExpected;
 
         Subtraction subtractionTest = new Subtraction();
         result = subtractionTest.performOperation(5,10);
+        resultEqualsExpected = (expectedResult == result);
 
-        Assertions.assertEquals(-5,result);
+        Assertions.assertTrue(expectedResult == result);
+        System.out.println("Subtraction test 5 - 10 equals -5: "+resultEqualsExpected);
     }
 
     @Test
     void testDivisionShouldReturnSix() {
         int result;
+        int expectedResult = 6;
+        boolean resultEqualsExpected;
 
         Division divideTest = new Division();
         result = divideTest.performOperation(12,2);
+        resultEqualsExpected = (expectedResult == result);
 
-        Assertions.assertEquals(6,result);
+        Assertions.assertTrue(expectedResult == result);
+        System.out.println("Division test 12/2 equals 6: "+resultEqualsExpected);
     }
 
 
@@ -61,21 +78,29 @@ class SimpleCalculatorTest {
     @Test
     void testMultiplicationShouldReturnTwentyFive() {
         int result;
+        int expectedResult = 25;
+        boolean resultEqualsExpected;
 
         Multiplication multiplyTest = new Multiplication();
         result = multiplyTest.performOperation(5,5);
+        resultEqualsExpected = (expectedResult == result);
 
-        Assertions.assertEquals(25,result);
+        Assertions.assertTrue(expectedResult == result);
+        System.out.println("Multiplication test 5*5 equals 25: "+resultEqualsExpected);
     }
 
     @Test
     void testMultiplicationShouldReturnNegativeTwo() {
         int result;
+        int expectedResult = -2;
+        boolean resultEqualsExpected;
 
         Multiplication multiplyTest = new Multiplication();
         result = multiplyTest.performOperation(-1,2);
+        resultEqualsExpected = (expectedResult == result);
 
-        Assertions.assertEquals(-2,result);
+        Assertions.assertTrue(expectedResult == result);
+        System.out.println("Multiplication test -1*2 equals -2: "+resultEqualsExpected);
     }
 
 }
